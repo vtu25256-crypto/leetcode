c1efb6f64f61cbb4a6169924f2a079d3cdf917b7
vtu25256-crypto <vtu25256@veltech.edu.in>
2026-01-23 11:12:19 +0530

    Time: 4 ms (84.80%) | Memory: 85.6 MB (64.24%) - LeetSync

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int prefixSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = nums[i] * i - prefixSum;
+            int right = (totalSum - prefixSum - nums[i]) 
+                        - nums[i] * (n - i - 1);
+
+            result[i] = left + right;
+            prefixSum += nums[i];
+        }
+
+        return result;
+    }
+}
