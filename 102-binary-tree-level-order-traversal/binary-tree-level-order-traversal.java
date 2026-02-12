98c1ca80ede3592a38c71322a3be4ca65185ca34
vtu25256-crypto <vtu25256@veltech.edu.in>
2026-02-12 20:01:46 +0530

    Time: 1 ms (95.55%) | Memory: 46.8 MB (79.14%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            for (int size = q.size(); size > 0; size--) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
