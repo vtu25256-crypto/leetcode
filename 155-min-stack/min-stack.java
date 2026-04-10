d3ff20a805e4ca47909e97ee745add04a942a3ed
vtu25256-crypto <vtu25256@veltech.edu.in>
2026-04-10 18:10:38 +0530

    Time: 5 ms (85.56%) | Memory: 47 MB (73.82%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,13 +1,8 @@
-import java.util.*;
 
 class MinStack {
-    Stack<long[]> st = new Stack<>();
-
-    public void push(int x) {
-        long min = st.isEmpty() ? x : Math.min(x, st.peek()[1]);
-        st.push(new long[]{x, min});
-    }
-    public void pop() { st.pop(); }
-    public int top() { return (int) st.peek()[0]; }
-    public int getMin() { return (int) st.peek()[1]; }
+    Stack<int[]> s=new Stack<>();
+    public void push(int x){s.push(new int[]{x,s.isEmpty()?x:Math.min(x,s.peek()[1])});}
+    public void pop(){s.pop();}
+    public int top(){return s.peek()[0];}
+    public int getMin(){return s.peek()[1];}
 }
